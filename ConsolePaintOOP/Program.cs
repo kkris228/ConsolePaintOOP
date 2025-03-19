de5b36fd93d68331a71ab08f3df692a9e6185ecb
kkris228 <butakovakristina6@gmail.com>
2025-03-19 22:24:26 +0300

    fix program.cs

--- ConsolePaintOOP/Program.cs
+++ ConsolePaintOOP/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsolePaintOOP.Terminal;
+
+Terminal terminal =
+                    args.Length != 0
+                    ? new(args[0])
+                    : new();
+
+terminal.Run();
+
+
